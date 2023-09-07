7c74bdbcbe2e15a971a98f9a7719f2a764221756
tom-sharp <dink521g@gmail.com>
2023-09-07 12:00:40 +0200

    Added initial CRandom examples

---
+++ Syslib.Core.Application/CRandom_Application.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Syslib.Core.Application
+{
+	internal class CRandom_Application
+	{
+
+		public void Run()
+		{
+			this.CustomSeedInit();      // Use of Custom Randomization init with a custom value (not needed)
+			this.RanomNumber();			// generate random positive integer numbers
+
+		}
+
+
+
+		void CustomSeedInit()
+		{
+			CRandom.Seed("Custom Init String");
+			Console.WriteLine("CRandom Custom Init");
+		}
+
+		void RanomNumber()
+		{
+			Console.WriteLine($"Random number () {CRandom.Random.RandomNumber()}");
+			Console.WriteLine($"Random number (Max 10000)  {CRandom.Random.RandomNumber(max: 10000)}");
+			Console.WriteLine($"Random number (100 - 999)  {CRandom.Random.RandomNumber(min: 100, max: 999)}");
+		}
+
+
+
+	}
+
+
+}
--- Syslib.Core.Application/Program.cs
+++ Syslib.Core.Application/Program.cs
@@ -1,4 +1,10 @@
 ﻿global using Syslib.Core.Application;
 global using Syslib;
 
+
+// CStr Examples
 new CStr_Application().Run();
+
+
+// CRandom Examples
+new CRandom_Application().Run();
