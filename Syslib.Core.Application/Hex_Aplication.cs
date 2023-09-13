76fad561827f45f2db8576d291dca15efe7166b8
tom-sharp <dink521g@gmail.com>
2023-09-13 15:45:21 +0200

    Initial Hex examples

---
+++ Syslib.Core.Application/Hex_Aplication.cs
@@ -0,0 +1,38 @@
+﻿
+namespace Syslib.Core.Application
+{
+	internal class Hex_Aplication
+	{
+
+
+		public void Run()
+		{
+			this.HexFromInt();
+			this.HexFromBuffer();
+
+		}
+
+
+		void HexFromInt()
+		{
+			int number = 10000;
+			var hex = Hex.FromInt(number);
+			Console.WriteLine($"Hex value from Integer {hex}");
+		}
+
+
+		void HexFromBuffer()
+		{
+			var buffer = new CStr("Hello World!").Str();
+
+			var hex = Hex.FromBuffer(buffer);
+
+			Console.WriteLine($"Hex value from Buffer {hex}");
+
+		}
+
+
+
+
+	}
+}
--- Syslib.Core.Application/Program.cs
+++ Syslib.Core.Application/Program.cs
@@ -12,3 +12,6 @@ new CRandom_Application().Run();
 // CList Examples
 new CList_Application().Run();
 
+
+// Hex Examples
+new Hex_Aplication().Run();
